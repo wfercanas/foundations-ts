7e552bf6ce63ecd577218c89a9cb7707cbfd8f5f
wfercanas <fernando.canas@outlook.com>
2022-10-05 17:41:10 -0500

    add/ numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,15 @@
+(() => {
+  let productPrice = 100;
+  productPrice = 12;
+  console.log('productPrice', productPrice);
+
+  let customerAge: number = 28;
+  customerAge = customerAge;
+  console.log('customerAge', customerAge);
+
+  let productInStock: number;
+  console.log('productInStock', productInStock);
+
+  let discount = parseInt('123');
+  console.log('discount', discount);
+})();
