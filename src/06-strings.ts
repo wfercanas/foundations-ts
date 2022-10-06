1c39c324503da9079bb0586723f4b5f575d0de17
wfercanas <fernando.canas@outlook.com>
2022-10-05 20:07:01 -0500

    add/ strings class

---
+++ src/06-strings.ts
@@ -0,0 +1,11 @@
+(() => {
+  let productTitle = 'My amazing product';
+  const productDescription = "Product's Description";
+  let summary = `
+    title: ${productTitle}
+    description: ${productDescription}
+  `;
+
+  console.log('productTitle', productTitle);
+  console.log('summary', summary);
+})();
